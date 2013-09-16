0e74fc38badfd10e7ca81d319b1cb16bd7d15447
Aleks Totic <a@totic.org>
2013-09-16 14:58:58 -0700

    Cleanup obsolete

--- release/release_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# extract with tar zxvf chromium_mac.tar.gz
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC=~/chromium/src/out/Release/
-DEST=$DIR/chromium_linux.tar.gz
-tar zcvf $DEST -C $SRC chrome chrome.pak resources.pak locales
-
